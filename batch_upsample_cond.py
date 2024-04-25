efe18e183ba4ac185fdd15954fa6f48a94433c69
Sx Lau <3211220990@qq,com>
2024-04-25 14:06:55 -0400

    Add diffusion model scripts

---
+++ batch_upsample_cond.py
@@ -0,0 +1,154 @@
+"""
+Generate a large batch of samples from a super resolution model, given a batch
+of samples from a regular model from image_sample.py.
+"""
+
+import argparse
+import os
+
+import blobfile as bf
+import numpy as np
+import torch as th
+import torch.distributed as dist
+import torch.nn.functional as F
+
+from guided_diffusion import dist_util, logger
+from guided_diffusion.script_util import (
+    NUM_CLASSES,
+    sr_model_and_diffusion_defaults,
+    sr_create_model_and_diffusion,
+    args_to_dict,
+    add_dict_to_argparser,
+    create_classifier,
+    classifier_defaults
+)
+
+
+def main():
+    args = create_argparser().parse_args()
+
+    dist_util.setup_dist()
+    logger.configure(dir='./results')
+
+    logger.log("creating model...")
+    model, diffusion = sr_create_model_and_diffusion(
+        **args_to_dict(args, sr_model_and_diffusion_defaults().keys())
+    )
+    model.load_state_dict(
+        dist_util.load_state_dict(args.model_path, map_location="cpu")
+    )
+    model.to(dist_util.dev())
+    if args.use_fp16:
+        model.convert_to_fp16()
+    model.eval()
+
+    # Additional codes
+    logger.log("loading classifier...")
+    classifier = create_classifier(**args_to_dict(args, classifier_defaults().keys()))
+    classifier.load_state_dict(
+        dist_util.load_state_dict(args.classifier_path, map_location="cpu")
+    )
+    classifier.to(dist_util.dev())
+    if args.classifier_use_fp16:
+        classifier.convert_to_fp16()
+    classifier.eval()
+
+    def cond_fn(x, t, low_res, y=None):
+        assert y is not None
+        with th.enable_grad():
+            x_in = x.detach().requires_grad_(True)
+            logits = classifier(x_in, t)
+            log_probs = F.log_softmax(logits, dim=-1)
+            selected = log_probs[range(len(logits)), y.view(-1)]
+            return th.autograd.grad(selected.sum(), x_in)[0] * args.classifier_scale
+        
+    def model_fn(x, t, low_res, y=None):
+        assert y is not None
+        return model(x, t, low_res, y=y)
+
+    logger.log("loading data...")
+    data = load_data_for_worker(args.base_samples, args.batch_size, args.class_cond)
+
+    logger.log("creating samples...")
+    all_images = []
+    while len(all_images) * args.batch_size < args.num_samples:
+        model_kwargs = next(data)
+        model_kwargs = {k: v.to(dist_util.dev()) for k, v in model_kwargs.items()}
+        sample = diffusion.p_sample_loop(
+            model_fn,
+            (args.batch_size, 3, args.large_size, args.large_size),
+            clip_denoised=args.clip_denoised,
+            model_kwargs=model_kwargs,
+            progress=True,
+
+            cond_fn=cond_fn,
+            device=dist_util.dev(),
+        )
+        sample = ((sample + 1) * 127.5).clamp(0, 255).to(th.uint8)
+        sample = sample.permute(0, 2, 3, 1)
+        sample = sample.contiguous()
+
+        all_samples = [th.zeros_like(sample) for _ in range(dist.get_world_size())]
+        dist.all_gather(all_samples, sample)  # gather not supported with NCCL
+        for sample in all_samples:
+            all_images.append(sample.cpu().numpy())
+        logger.log(f"created {len(all_images) * args.batch_size} samples")
+
+    arr = np.concatenate(all_images, axis=0)
+    arr = arr[: args.num_samples]
+    if dist.get_rank() == 0:
+        shape_str = "x".join([str(x) for x in arr.shape])
+        out_path = os.path.join(logger.get_dir(), f"samples_{shape_str}.npz")
+        logger.log(f"saving to {out_path}")
+        np.savez(out_path, arr)
+
+    dist.barrier()
+    logger.log("sampling complete")
+
+
+def load_data_for_worker(base_samples, batch_size, class_cond):
+    with bf.BlobFile(base_samples, "rb") as f:
+        obj = np.load(f)
+        image_arr = obj["arr_0"]
+        if class_cond:
+            label_arr = obj["arr_1"]
+    rank = dist.get_rank()
+    num_ranks = dist.get_world_size()
+    buffer = []
+    label_buffer = []
+    while True:
+        for i in range(rank, len(image_arr), num_ranks):
+            buffer.append(image_arr[i])
+            if class_cond:
+                label_buffer.append(label_arr[i])
+            if len(buffer) == batch_size:
+                batch = th.from_numpy(np.stack(buffer)).float()
+                batch = batch / 127.5 - 1.0
+                batch = batch.permute(0, 3, 1, 2)
+                res = dict(low_res=batch)
+                if class_cond:
+                    res["y"] = th.from_numpy(np.stack(label_buffer))
+                yield res
+                buffer, label_buffer = [], []
+
+
+def create_argparser():
+    defaults = dict(
+        clip_denoised=True,
+        num_samples=10000,
+        batch_size=16,
+        use_ddim=False,
+        base_samples="",
+        model_path="",
+        classifier_path="",
+        classifier_scale=1.0,
+    )
+    defaults.update(sr_model_and_diffusion_defaults())
+    defaults.update(classifier_defaults())
+    parser = argparse.ArgumentParser()
+    add_dict_to_argparser(parser, defaults)
+    return parser
+
+
+if __name__ == "__main__":
+    main()
